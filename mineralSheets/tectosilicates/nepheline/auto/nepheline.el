fdeb8a0b2d2bf31adfd2f33ee7072035e6ee7204
Benjamin Bass <wesleybass42@gmail.com>
2016-02-27 04:04:53 -0500

    Version of tectosilicates lab to turn in. Added highlighting package

---
+++ mineralSheets/tectosilicates/nepheline/auto/nepheline.el
@@ -0,0 +1,30 @@
+(TeX-add-style-hook
+ "nepheline"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "10pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ulem" "normalem") ("mhchem" "version=3")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "hyperref"
+    "graphicx"
+    "frame"
+    "color"
+    "framed"
+    "minibox"
+    "mhchem"))
+ :latex)
+
