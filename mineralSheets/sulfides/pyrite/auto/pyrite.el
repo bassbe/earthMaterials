03e8590fbe0707dfeebcf81bfc20cb874d013fa6
Benjamin Bass <wesleybass42@gmail.com>
2016-03-30 05:46:02 -0400

    Add carbonates, sulfides, and sulfates to mineralsheets and picbank

---
+++ mineralSheets/sulfides/pyrite/auto/pyrite.el
@@ -0,0 +1,31 @@
+(TeX-add-style-hook
+ "pyrite"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "10pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ulem" "normalem") ("mhchem" "version=3")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "hyperref"
+    "color"
+    "soul"
+    "graphicx"
+    "frame"
+    "framed"
+    "minibox"
+    "mhchem"))
+ :latex)
+
